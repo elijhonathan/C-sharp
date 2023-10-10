dc88e24ac8a5f10a22c94b4ee48d527c9a7bcfbe
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-10 20:11:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using C_.Models;
+
+int a = 0;
+int estoque = 7;
+
+Console.Write("Digite a quantidade que deseja comprar: ");
+a = int.Parse(Console.ReadLine());
+
+if (a <= estoque)
+{
+    Console.WriteLine("Compra realizada com sucesso!");
+}
+
+else
+{
+    Console.WriteLine("Desculpe, no momento não temos a quantidade necessaria do produto.");
+}
