441d71d70d0bbe5e572033614aa95a312ddb588a
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-10 18:10:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using C_.Models;
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+
+
+
+
+
+
+
+//Pessoa p = new Pessoa();
+
+//p.Nome = "Eli";
+//p.Idade = 31;
+//p.Apresentar();
+
