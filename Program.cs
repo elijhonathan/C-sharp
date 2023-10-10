f1049dd2c50007c566ce87f74e9a4fe002361417
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-10 19:53:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using C_.Models;
+
+string a = "";
+Console.WriteLine("Digite um numéro: ");
+a = Console.ReadLine();
+
+ int b = 0;
+
+ int.TryParse(a, out b);
+
+
+ Console.WriteLine(b);
+Console.WriteLine("Conversão realizada com sucesso");
+
+
+
+
+
+
+
+
+
+
+
+// DateTime dataAtual = DateTime.Now;
+// Console.WriteLine(dataAtual);
+
+
+
+
+
+
+
+//Pessoa p = new Pessoa();
+
+//p.Nome = "Eli";
+//p.Idade = 31;
+//p.Apresentar();
+
