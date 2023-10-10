d93f891dea9755d3a688fd722b777c9c0f377a07
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-10 20:39:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using C_.Models;
+
+Console.WriteLine("Digite uma letra: ");
+string letra = Console.ReadLine();
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("É uma vogal!");
+        break;
+
+    default:
+        Console.WriteLine("É uma consoante!");
+        break;
+}
