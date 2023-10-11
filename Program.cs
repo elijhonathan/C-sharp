a21c07e9a093801fdad573b552da9e763ec373cc
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-11 17:34:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using C_.Models;
+
+
+int numeroIncremento = 10;
+int numeroDecremento = 20;
+
+Console.WriteLine(numeroIncremento);
+Console.WriteLine($"Incrementando o número {numeroIncremento}");
+numeroIncremento++;
+Console.WriteLine(numeroIncremento);
+
+Console.WriteLine(numeroDecremento);
+Console.WriteLine($"Decrementando o número {numeroDecremento}");
+numeroDecremento--;
+Console.WriteLine(numeroDecremento);
+
+
