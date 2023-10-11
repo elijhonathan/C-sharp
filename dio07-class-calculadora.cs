5c9a856b4996b9ce95e69ad4461ae4919b1ec4ca
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-11 17:38:23 -0300

    Update dio07-class-calculadora.cs

--- dio07-class-calculadora.cs
+++ dio07-class-calculadora.cs
@@ -55,6 +55,12 @@ namespace C_.Models
             Console.WriteLine($"Tangente de {angulo}° = {Math.Round(tangente, 4)}° ");
         }
 
+        public void RaizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quadrada de {x} = {raiz}");
+        }
+
 
     }
 
