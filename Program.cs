9fef58196b878b6e59645a080e605a28242bcb52
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-12 14:02:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using C_.Models;
+
+
+int soma = 0, numero = 0;
+
+
+do
+{
+    Console.WriteLine("Digite um número para somar (Digite 0 para parar): ");
+    numero = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{numero} + {soma} = {numero + soma}");
+    soma += numero;
+    
+
+
+} while(numero != 0);
+
+
+
+
+
+
+
+
