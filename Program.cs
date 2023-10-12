878b135b59184dd3473dd801e7b435129163ef6c
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-12 13:44:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using C_.Models;
+
+
+int numero = 3;
+int contador = 1;
+
+while (contador <=10)
+{
+    Console.WriteLine($"{numero} * {contador} = {numero * contador} ");
+    contador++;
+}
+
+
+
+
+
