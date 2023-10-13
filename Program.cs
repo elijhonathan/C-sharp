eba42ad33a87bd465cb9eb110f2b7d684480ab04
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-13 19:14:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using C_.Common.Models;
+
+List<string> listaString = new List<string>();
+
+listaString.Add("SP");
+listaString.Add("MT");
+listaString.Add("MS");
+listaString.Add("PR");
+listaString.Add("SP");
+
+Console.WriteLine("Percorrendo a lista com FOR:");
+for(int contador = 0; contador < listaString.Count; contador++)
+{
+    Console.WriteLine($"Posição N° {contador} - {listaString[contador]}");
+}
+
+
+Console.WriteLine("Percorrendo a lista com o FOREACH:");
+int contadorForeach = 0;
+foreach(string item in listaString)
+{
+    Console.WriteLine($"Posição N° {contadorForeach} - {item}");
+    contadorForeach++;
+}
+
+
+
+
+// Pessoa pessoa1 = new Pessoa();
+// pessoa1.Nome = "Eli";
+// pessoa1.Idade = 29;
+// pessoa1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+
