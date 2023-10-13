785ef45c5e8b93eab88e1a4d08399fdef07866f4
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-13 18:40:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using C_.Common.Models;
+
+int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+
+foreach(int valor in arrayInteiros)
+{
+    Console.WriteLine(valor);
+}
+
+
+
+
+
+// Pessoa pessoa1 = new Pessoa();
+// pessoa1.Nome = "Eli";
+// pessoa1.Idade = 29;
+// pessoa1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+
