cdc55dc6646d7b58f38bc1d53b215e2d100b9f2e
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-13 18:44:24 -0300

    Update dio17-array-foreach.cs

--- dio17-array-foreach.cs
+++ dio17-array-foreach.cs
@@ -6,8 +6,10 @@ arrayInteiros[0] = 72;
 arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 
+int contadorForeach = 0;
+
 foreach(int valor in arrayInteiros)
 {
-    Console.WriteLine(valor);
+    Console.WriteLine($"Posição N° {contadorForeach} - {valor}");
+    contadorForeach++;
 }
-
