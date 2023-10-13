b5823dae397bcb9bfbb5b85cb4f78c1db1210aa8
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-13 18:35:19 -0300

    Update and rename Program.cs to dio16-array.cs

--- dio16-array.cs
+++ dio16-array.cs
@@ -10,26 +10,3 @@ for(int contador = 0; contador < arrayInteiros.Length; contador++)
 {
     Console.WriteLine($"Posição N° {contador} - {arrayInteiros[contador]}");
 }
-
-
-
-
-
-
-
-
-// Pessoa pessoa1 = new Pessoa();
-// pessoa1.Nome = "Eli";
-// pessoa1.Idade = 29;
-// pessoa1.Apresentar();
-
-
-
-
-
-
-
-
-
-
-
