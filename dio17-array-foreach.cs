e1a54f886e3325d50ade11cac6d1ab34a29b90a9
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-13 18:41:08 -0300

    Update and rename Program.cs to dio17-array-foreach.cs

--- dio17-array-foreach.cs
+++ dio17-array-foreach.cs
@@ -11,22 +11,3 @@ foreach(int valor in arrayInteiros)
     Console.WriteLine(valor);
 }
 
-
-
-
-
-// Pessoa pessoa1 = new Pessoa();
-// pessoa1.Nome = "Eli";
-// pessoa1.Idade = 29;
-// pessoa1.Apresentar();
-
-
-
-
-
-
-
-
-
-
-
