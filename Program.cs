1a9012e8bd4ac9f678626c3fdd5b3d9bc6facee7
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-14 20:05:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using C_.Common.Models;
+
+using System;
+
+namespace AvaliacaoTestesAutomatizados {
+  class Program {
+    static void Main(string[] args) {
+      // Solicita ao usuário a entrada para o número de testes passados
+      int testesPassados = Convert.ToInt32(Console.ReadLine());
+
+      // Solicita ao usuário a entrada para o número total de testes
+      int totalTestes = Convert.ToInt32(Console.ReadLine());
+
+      // TODO: Calcule a taxa de sucesso e armazená-la na variável taxaSucesso:
+      double taxaSucesso = ((double)testesPassados / totalTestes) * 100;
+
+      // Exibe a taxa de sucesso com 2 casas decimais
+      Console.WriteLine($"Taxa de sucesso: {taxaSucesso:F2}%");
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa pessoa1 = new Pessoa();
+// pessoa1.Nome = "Eli";
+// pessoa1.Idade = 29;
+// pessoa1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+
