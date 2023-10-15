d71e34fd8030cbcb390aecb2fecb0ee57f69dff2
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-15 19:41:02 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using ExemplosExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Eli";
+p1.Sobrenome = "Jhonathan";
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Jheniffer";
+p2.Sobrenome = "Tille";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Eli";
+// p1.Sobrenome = "Jhonathan";
+// p1.Idade = 31;
+// p1.Apresentar();
