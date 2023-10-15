0cecf944c95cf6e433cd7a0a22afcfd4f206023a
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-15 19:41:37 -0300

    Update and rename Program.cs to dio27-Usando-Classe-Curso.cs

--- dio27-Usando-Classe-Curso.cs
+++ dio27-Usando-Classe-Curso.cs
@@ -16,17 +16,3 @@ cursoDeIngles.Alunos = new List<Pessoa>();
 cursoDeIngles.AdicionarAluno(p1);
 cursoDeIngles.AdicionarAluno(p2);
 cursoDeIngles.ListarAlunos();
-
-
-
-
-
-
-
-
-
-// Pessoa p1 = new Pessoa();
-// p1.Nome = "Eli";
-// p1.Sobrenome = "Jhonathan";
-// p1.Idade = 31;
-// p1.Apresentar();
