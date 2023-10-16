b35e91dbc85919576e450294d002392ee2c22dfe
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-16 20:13:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using ExemplosExplorando.Models;
+using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
+decimal valorMonetario = 1582.40m;
+
+Console.WriteLine($"{valorMonetario:C}");
+
+
+
