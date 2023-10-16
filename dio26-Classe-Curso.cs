789eff04b8f01a38ccbb340140b5ea1c5c481413
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-16 19:50:33 -0300

    Update dio26-Classe-Curso.cs

--- dio26-Classe-Curso.cs
+++ dio26-Classe-Curso.cs
@@ -33,7 +33,7 @@ namespace ExemplosExplorando.Models
             for (int count = 0; count < Alunos.Count; count++)
             {
                 //string texto = "N° " + count + " - " + Alunos[count].NomeCompleto;
-                string texto = $"N° {count} - {Alunos[count].NomeCompleto}";
+                string texto = $"N° {count + 1} - {Alunos[count].NomeCompleto}";
                 Console.WriteLine(texto);  
                 
             }
