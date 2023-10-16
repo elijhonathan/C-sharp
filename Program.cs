4a843c4e7e0984d271a1fa5f124108ca83213085
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-16 20:35:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ExemplosExplorando.Models;
+
+
+
+DateTime data = DateTime.Now;
+
+Console.WriteLine(data);
+
+Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
+
+Console.WriteLine(data.ToString("dd/MM/yy HH:mm"));
+
+Console.WriteLine(data.ToString("HH:mm dd/MM/yy"));
+
+
+
+
