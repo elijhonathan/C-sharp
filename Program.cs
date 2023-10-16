b1f3e098281b85a1c041e031798da66f82dd0983
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-16 19:59:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ExemplosExplorando.Models;
+
+string numero1 = "10";
+string numero2 = "20";
+
+string resultado = numero1 + numero2;
+
+Console.WriteLine(resultado);
+
+int numero11 = 10;
+int numero22 = 20;
+
+int resultadoo = numero11 + numero22;
+
+Console.WriteLine(resultadoo);
+
+
