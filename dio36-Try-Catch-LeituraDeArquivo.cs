aa09acbedfd0361692db1519dfe195d1d3131812
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-17 20:11:27 -0300

    Update dio36-Try-Catch-LeituraDeArquivo.cs

--- dio36-Try-Catch-LeituraDeArquivo.cs
+++ dio36-Try-Catch-LeituraDeArquivo.cs
@@ -1,25 +1,28 @@
-﻿using ExemplosExplorando.Models;
+using ExemplosExplorando.Models;
 
 try
 {
 
     string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+    //string[] linhas = File.ReadAllLines("Arquivos/arquivoxxLeitura.txt");
+    //string[] linhas = File.ReadAllLines("Arquivos/mundo/arquivoLeitura.txt");
 
     foreach (string linha in linhas)
     {
         Console.WriteLine(linha);
     }
-} catch(Exception ex)
+} 
+catch(FileNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado {ex.Message}");
+}
+catch(DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Pasta não encontrada {ex.Message}");
+}
+catch(Exception ex)
 {
     Console.WriteLine($"Ocorreu uma exceção generica. {ex.Message}");
 }
 
-
-
-
-
-
-
-
-
-
+Console.WriteLine("Chegou até aqui!");
