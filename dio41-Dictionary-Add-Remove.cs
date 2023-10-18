1f4aa522b47858558d6889305961bd4bddd1fea8
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-17 21:19:29 -0300

    Update dio41-Dictionary-Add-Remove.cs

--- dio41-Dictionary-Add-Remove.cs
+++ dio41-Dictionary-Add-Remove.cs
@@ -1,4 +1,4 @@
-﻿using ExemplosExplorando.Models;
+using ExemplosExplorando.Models;
 
 Dictionary<string, string> estados = new Dictionary<string, string>();
 
@@ -8,6 +8,22 @@ estados.Add("PR", "Paraná");
 estados.Add("MT", "Mato Grosso");
 
 //foreach (KeyValuePair<string, string> item in estados)
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+estados.Add("SC", "Santa Catarina");
+Console.WriteLine("Nova lista");
+
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+estados.Remove("SP");
+Console.WriteLine("Nova lista");
+
 foreach (var item in estados)
 {
     Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
