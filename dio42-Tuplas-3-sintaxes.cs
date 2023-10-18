7fa3b9b963467af4a06dc04ba25b82c115d96189
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-18 20:58:46 -0300

    Update and rename dio42-Tuplas-2sintaxes.cs to dio42-Tuplas-3-sintaxes.cs

--- dio42-Tuplas-3-sintaxes.cs
+++ dio42-Tuplas-3-sintaxes.cs
@@ -1,7 +1,10 @@
 using ExemplosExplorando.Models;
 
-(int, string, string, decimal) tupla = (1, "Eli", "Jhonathan", 9.5M);
+(int ID, string Nome, string Sobrenome, decimal Nota) tupla = (1, "Eli", "Jhonathan", 9.5M);
+
 ValueTuple<int, string, string, decimal>  tupla2 = (2, "Jhennifer", "Tilly", 10.0M);
+
+var tupla3 = Tuple.Create(3, "Gwen", "Tilly", 8.9M);
 
 Console.WriteLine($"ID: {tupla.Item1}");
 Console.WriteLine($"Nome: {tupla.Item2}");
@@ -12,3 +15,8 @@ Console.WriteLine($"ID: {tupla2.Item1}");
 Console.WriteLine($"Nome: {tupla2.Item2}");
 Console.WriteLine($"Sobrenome: {tupla2.Item3}");
 Console.WriteLine($"Nota: {tupla2.Item4}");
+
+Console.WriteLine($"ID: {tupla3.Item1}");
+Console.WriteLine($"Nome: {tupla3.Item2}");
+Console.WriteLine($"Sobrenome: {tupla3.Item3}");
+Console.WriteLine($"Nota: {tupla3.Item4}");
