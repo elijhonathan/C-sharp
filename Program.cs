ce459c7d04787e522c6ba1fad22dafe9549fff70
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-17 21:13:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using ExemplosExplorando.Models;
+
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("MS", "Mato Grosso do Sul");
+estados.Add("PR", "Paraná");
+estados.Add("MT", "Mato Grosso");
+
+//foreach (KeyValuePair<string, string> item in estados)
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
