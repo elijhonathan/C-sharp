65fcc426ff4ccf5aa2acfa6882b2b1d3f29909fb
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-19 15:09:13 -0300

    Update dio49-Serializacao-Json.cs

--- dio49-Serializacao-Json.cs
+++ dio49-Serializacao-Json.cs
@@ -3,6 +3,6 @@ using Newtonsoft.Json;
 
 Vendas v1 = new Vendas(1, "Material de escritorio", 25.50M);
 
-string serializado = JsonConvert.SerializeObject(v1);
+string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
 
 Console.WriteLine(serializado);
