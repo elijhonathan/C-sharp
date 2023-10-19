6d236443ae3934ee0d6046eb7e36a977c38080a3
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-19 14:24:56 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using ExemplosExplorando.Models;
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        // Leia os valores inteiros A e B
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        // Realize a soma e atribua o resultado a SOMA
+        int SOMA = A + B;
+
+        // Imprima o resultado com uma quebra de linha no final
+        Console.WriteLine("SOMA = " + SOMA);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
