308c4081c283db678303a42e9fc436957314fff4
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-18 21:46:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using ExemplosExplorando.Models;
+
+int numero = 21;
+int numero2 = 20;
+bool ehPar = false;
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine($"O número {numero} é par.");
+}
+else
+{
+    Console.WriteLine($"O número {numero} é ímpar.");
+}
+
+//IF Ternário
+ehPar = numero2 % 2 == 0;
+Console.WriteLine($"O número {numero2} é " + (ehPar ? "par" : "ímpar"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
