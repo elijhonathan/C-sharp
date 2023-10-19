5e6966be873310722341647e0a7b7a491fead4fe
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-18 21:20:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using ExemplosExplorando.Models;
+
+LeituraArquivo arquivo = new LeituraArquivo();
+
+var (sucesso, linhasArquivo, quantidadeLinhas) = arquivo.LerArquivo("Arquivos/arquivoLeitura.txt");
+//var (sucesso, linhasArquivo, quantidadeLinhas) = arquivo.LerArquivo("Arquivoxs/arquivoLeitura.txt");
+
+if (sucesso)
+{
+    Console.WriteLine($"Quantidade de linhas do arquivo: {quantidadeLinhas}");
+    foreach(string linha in linhasArquivo)
+    {
+        Console.WriteLine(linha);
+    }
+}
+else
+{
+    Console.WriteLine("Não foi possivel ler o arquivo");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
