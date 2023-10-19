8d1b9003a6f4b398f6cc4c0e17d9fac1b0bc1c3e
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-18 21:35:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using ExemplosExplorando.Models;
+
+Pessoa p1 = new Pessoa("Eli", "Jhonathan");
+
+(string nome, string sobrenome) = p1;
+
+Console.WriteLine($"{nome} {sobrenome}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
