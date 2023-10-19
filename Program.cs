122b56a3aedb0df250d4a6c7380a340cdd97c4c0
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-19 15:24:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using ExemplosExplorando.Models;
+using Newtonsoft.Json;
+
+List<Vendas> listaVendas = new List<Vendas>();
+
+Vendas v1 = new Vendas(1, "Material de escritorio", 25.50M);
+Vendas v2 = new Vendas(2, "Licensa de software", 110.00M);
+
+listaVendas.Add(v1);
+listaVendas.Add(v2);
+
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+File.WriteAllText("Arquivos/vendas-colecao.json", serializado);
+
+Console.WriteLine(serializado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
