2c71854ef02f228a32c72d8b1ab1b1b82aef8939
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-19 15:05:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using ExemplosExplorando.Models;
+using Newtonsoft.Json;
+
+Vendas v1 = new Vendas(1, "Material de escritorio", 25.50M);
+
+string serializado = JsonConvert.SerializeObject(v1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
