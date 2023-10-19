d7c69a0348ff9813552523257b56a20ad5addb36
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-19 13:27:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using ExemplosExplorando.Models;
+
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+        string entradaA = Console.ReadLine();
+        string entradaB = Console.ReadLine();
+
+        int A = int.Parse(entradaA);
+        int B = int.Parse(entradaB);
+
+        
+        int X = A + B;
+        
+        Console.WriteLine($"x = {X}");
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
