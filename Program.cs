3e81c0036e06b00509a0851564d31cb4beb0790f
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-20 21:06:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using ExemplosExplorando.Models;
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lê o valor do raio
+        double raio = Convert.ToDouble(Console.ReadLine());
+
+        // Calcula a área da circunferência
+        double pi = 3.14159;
+        double area = pi * raio * raio;
+
+        // Exibe o resultado com 4 casas decimais
+        Console.WriteLine($"A={area:F4}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
