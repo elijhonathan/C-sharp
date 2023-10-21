2d43d8a44cfcc2d9395a12a20aec15a8717fcc59
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-20 21:08:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using ExemplosExplorando.Models;
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lê os dois valores inteiros
+        int valor1 = Convert.ToInt32(Console.ReadLine());
+        int valor2 = Convert.ToInt32(Console.ReadLine());
+
+        // Calcula o produto
+        int produto = valor1 * valor2;
+
+        // Exibe o resultado
+        Console.WriteLine($"PROD = {produto}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
