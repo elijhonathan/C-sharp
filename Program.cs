8d23e948d86dea226430e393d5875c112e39c05a
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-20 21:13:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using ExemplosExplorando.Models;
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lê as três notas
+        double notaA = Convert.ToDouble(Console.ReadLine());
+        double notaB = Convert.ToDouble(Console.ReadLine());
+        double notaC = Convert.ToDouble(Console.ReadLine());
+
+        // Define os pesos das notas
+        double pesoA = 2.0;
+        double pesoB = 3.0;
+        double pesoC = 5.0;
+
+        // Calcula a média ponderada
+        double media = (notaA * pesoA + notaB * pesoB + notaC * pesoC) / (pesoA + pesoB + pesoC);
+
+        // Exibe o resultado formatado
+        Console.WriteLine($"MEDIA = {media:F1}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
