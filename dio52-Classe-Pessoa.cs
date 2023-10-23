07703e05c26647250597f38bf14e8429bcfee1ac
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-23 17:39:46 -0300

    Update dio52-Classe-Pessoa.cs

--- dio52-Classe-Pessoa.cs
+++ dio52-Classe-Pessoa.cs
@@ -10,7 +10,7 @@ namespace ExemploPoo.Models
         public string Nome { get; set; }
         public int Idade { get; set; }
 
-        public virtual void Apresentar()
+        public void Apresentar()
         {
             Console.WriteLine($"Olá meu nome é {Nome} e tenho {Idade} de anos.");
         }
