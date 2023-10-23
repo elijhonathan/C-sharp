7e14eff6bf2045c41ea91dc6527cd379666e2702
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-23 16:34:47 -0300

    Update ProjetoPessoal01-calculador-de-dias.cs

--- ProjetoPessoal01-calculador-de-dias.cs
+++ ProjetoPessoal01-calculador-de-dias.cs
@@ -18,11 +18,13 @@ class Program
             TimeSpan diferenca = data - dataAtual;
 
             // Calcula o resultado multiplicando a diferença em dias por 3.5
+            // Este 3,5 é um calculo de porcentagem de progesso em curso, sendo a meta avançar 3,5% do curso por dia
+            // EX. um curso de 110 horas com um prazo de 32 dias para acabar 110 / 32 == 3,4375...
             double resultado = diferenca.TotalDays * 3.5;
 
             // Exibe o resultado
             Console.WriteLine($"A diferença em dias é: {diferenca.TotalDays:F2}");
-            Console.WriteLine($"O resultado da multiplicação é: {resultado:F2}");
+            Console.WriteLine($"O resultado da multiplicação é: {resultado:F2} %");
         }
         else
         {
