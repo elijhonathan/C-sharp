5257e439e5059cd673ef6954a3a6b584c7df747a
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-23 17:44:02 -0300

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPoo.Models
+{
+    public class Aluno : Pessoa
+    {
+        public double Nota { get; set; }
+
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos, e sou um aluno(a) nota {Nota}");
+        }
+
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using ExemploPoo.Models;
+
+Aluno a1 = new Aluno();
+Professor p1 = new Professor();
+
+a1.Nome = "Eli";
+a1.Idade = 31;
+a1.Nota = 9.5;
+a1.Apresentar();
+
+p1.Nome = "Ludmila";
+p1.Idade = 55;
+p1.Salario = 3500.00M;
+p1.Apresentar();
+
+
