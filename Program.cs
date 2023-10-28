2511a9ae0b8c2e5aac5becba09b37a7fc3682af1
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-10-28 20:28:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using Calculadora.Services;
+
+
+CalculadoraImprementacao c = new CalculadoraImprementacao();
+
+
+int num1 = 5;
+int num2 = 10;
+
+Console.WriteLine($"{num1} + {num2} = {c.Somar(num1, num2)}");
