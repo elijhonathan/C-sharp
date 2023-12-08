ca4d15245fe6fa4ac83d0ea2e15b6642b4c582a6
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-12-07 22:26:05 -0300

    Create dioAvanade03-Desafio-For.cs

---
+++ dioAvanade03-Desafio-For.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita ao usuário os limites inferiores e superiores
+        int limiteInferior = int.Parse(Console.ReadLine());
+        int limiteSuperior = int.Parse(Console.ReadLine());
+
+        // Variável para acumular a soma dos números pares
+        int somaPares = 0;
+
+        // TODO: Crie um Loop para percorrer os números no intervalo
+        for (int i = limiteInferior; i <= limiteSuperior; i++)
+        {
+            // TODO: Implemente o if para verificar se o número é par:
+            if (i % 2 == 0)
+            {
+                // TODO: Crie o acumulador para a soma dos números pares:
+                somaPares += i;
+            }
+        }
+
+        // Exibe o resultado
+        Console.WriteLine($"A soma dos numeros pares de {limiteInferior} a {limiteSuperior} e: {somaPares}");
+    }
+}
