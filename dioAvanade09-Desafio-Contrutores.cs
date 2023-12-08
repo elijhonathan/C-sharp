6914121ad261972110875edffebda1dc1c59552f
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-12-07 22:55:49 -0300

    Create dioAvanade09-Desafio-Contrutores.cs

---
+++ dioAvanade09-Desafio-Contrutores.cs
@@ -0,0 +1,49 @@
+using System;
+
+class Personagem
+{
+    public string Nome { get; set; }
+    public string Raca { get; set; }
+    public string Classe { get; set; }
+    public int Nivel { get; set; } = 1;
+    public int Vida { get; set; } = 10;
+
+    public Personagem(string nome, string raca, string classe)
+    {
+        Nome = nome;
+        Raca = raca;
+        Classe = classe;
+    }
+
+    public void ExibirStatus()
+    {
+        Console.WriteLine("Nome:" + Nome);
+        Console.WriteLine("Raça:" + Raca);
+        Console.WriteLine("Classe:" + Classe);
+        Console.WriteLine("Nível:" + Nivel );
+        Console.WriteLine("Vida:" + Vida);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita ao usuário que insira as informações do personagem
+
+        string nome = Console.ReadLine();
+
+
+        string raca = Console.ReadLine();
+
+
+        string classe = Console.ReadLine();
+
+        // Cria um objeto Personagem com base nas informações fornecidas pelo usuário
+        Personagem personagem = new Personagem(nome, raca, classe);
+
+        // Chama o método para exibir o status do personagem
+        Console.WriteLine("Status:");
+        personagem.ExibirStatus();
+    }
+}
